b4fd02b6d2a867c040fb02257a96aaf1dc990748
Ljaewon-123 <jml6388@naver.com>
2025-06-30 12:59:33 +0900

    config: primevue tailwindcssv4

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>Hello world</h1>
+  <h1 class="p-4">Hello world</h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primeuix/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
+  css: ['~/assets/styles/main.css'],
+  modules: [
+    '@nuxt/fonts',
+    'nuxt-security',
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+    '@nuxtjs/color-mode'
+  ],
+  primevue: {
+    options: {
+      ripple: true,
+      inputVariant: 'filled',
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'system',
+        }
+      }
+    }
+  }
 })
