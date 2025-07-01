d1fc080385c3a9c5a2bf3f04bd3b4819106c4de8
Ljaewon-123 <jml6388@naver.com>
2025-07-01 17:34:54 +0900

    design: how to use cursor style

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,38 +1,47 @@
 <template>
   <div>
     <header>
-      <Toolbar>
+      <Menubar>
         <template #start>
-            <Button icon="pi pi-plus" class="mr-2" severity="secondary" text />
-            <Button icon="pi pi-print" class="mr-2" severity="secondary" text />
-            <Button icon="pi pi-upload" severity="secondary" text />
+          We Need Main Icon!!
+          <!-- <span class="cursor-pointer">
+            되는 클래스인데 primevue에 적용이 안되네 
+          </span> -->
         </template>
 
-        <template #center>
-            <IconField>
-                <InputIcon>
-                    <i class="pi pi-search" />
-                </InputIcon>
-                <InputText placeholder="Search" />
-            </IconField>
+        <template #end> 
+          <div class="flex items-center gap-2">
+            <Button class="cursor-pointer!" icon="pi pi-home" size="small" variant="outlined" aria-label="Home" />
+            <Button icon="pi pi-comments" size="small" variant="outlined" aria-label="Comments" />
+            <Button icon="pi pi-github" size="small" variant="outlined" aria-label="Github" />
+            <Select 
+              v-model="version"
+              :options="versions" 
+              optionLabel="name" 
+              size="small" 
+              placeholder="Now version" 
+              :pt="{
+                // root: { style: { cursor: 'pointer'} },
+                // label: { style: { cursor: 'pointer'} },
+                // dropdown: { style: { cursor: 'pointer'} },
+              }"
+            >
+              <template #option="{ option }">
+                {{ option.code }}
+              </template>
+            </Select>
+          </div>
         </template>
-
-        <template #end> <SplitButton label="Save" :model="items"></SplitButton></template>
-    </Toolbar>
+    </Menubar>
     </header>
     <slot/>
   </div>
 </template>
 
 <script setup lang="ts">
-const items = ref([
-    {
-        label: 'Update',
-        icon: 'pi pi-refresh'
-    },
-    {
-        label: 'Delete',
-        icon: 'pi pi-times'
-    }
-])
+const version = ref({ name: 'V1', code: 'Vsersion 1' });
+const versions = ref([
+  { name: 'V1', code: 'Vsersion 1' },
+]);
 </script>
+
