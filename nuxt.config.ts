db637da46c175d8ac04135b781b7069a6ff042d4
Ljaewon-123 <jml6388@naver.com>
2025-07-02 14:38:31 +0900

    config: inspiraui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,13 +5,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
-  css: ['~/assets/styles/main.css'],
+  css: [
+    '~/assets/styles/main.css',
+    '~/assets/tyles/inspira.css'
+  ],
   modules: [
     '@nuxt/fonts',
     'nuxt-security',
     '@nuxtjs/tailwindcss',
     '@primevue/nuxt-module',
-    '@nuxtjs/color-mode'
+    '@nuxtjs/color-mode',
+    '@vueuse/nuxt'
   ],
   primevue: {
     options: {
