3d28d165d166e60473f1749b514fd199c1fe456b
Ljaewon-123 <jml6388@naver.com>
2025-07-03 17:38:40 +0900

    design: blur효과

--- app/components/ui/interactive-hover-button/InteractiveHoverButton.vue
+++ app/components/ui/interactive-hover-button/InteractiveHoverButton.vue
@@ -3,14 +3,14 @@
     ref="buttonRef"
     :class="
       cn(
-        'group relative w-auto cursor-pointer overflow-hidden rounded-full border bg-background p-2 px-6 text-center font-semibold',
+        'group relative w-auto cursor-pointer overflow-hidden rounded-full border bg-background dark:bg-gray-800  p-2 px-6 text-center font-semibold bg-white/20 backdrop-blur-lg dark:bg-gray-900/20 dark:backdrop-blur-lg text-foreground dark:text-gray-100 border-gray-300 dark:border-gray-600',
         props.class,
       )
     "
   >
     <div class="flex items-center gap-2">
       <div
-        class="size-2 scale-100 rounded-lg bg-primary transition-all duration-300 group-hover:scale-[100.8]"
+        class="size-2 scale-100 rounded-lg bg-primary dark:bg-white transition-all duration-300 group-hover:scale-[100.8]"
       ></div>
       <span
         class="inline-block whitespace-nowrap transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0"
@@ -20,7 +20,7 @@
     </div>
 
     <div
-      class="absolute top-0 z-10 flex size-full translate-x-12 items-center justify-center gap-2 text-primary-foreground opacity-0 transition-all duration-300 group-hover:-translate-x-5 group-hover:opacity-100"
+      class="absolute top-0 z-10 flex size-full translate-x-12 items-center justify-center gap-2 text-primary-foreground dark:text-black opacity-0 transition-all duration-300 group-hover:-translate-x-5 group-hover:opacity-100"
     >
       <span class="whitespace-nowrap">{{ text }}</span>
       <svg
