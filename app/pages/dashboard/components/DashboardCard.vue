8769b5d1396ae4c42feb00f852417d876dc62b34
Ljaewon-123 <jml6388@naver.com>
2025-07-16 15:21:43 +0900

    design: dashboard index page

--- app/pages/dashboard/components/DashboardCard.vue
+++ app/pages/dashboard/components/DashboardCard.vue
@@ -7,16 +7,20 @@ const { title = 'Title', subtitle = 'Subtitle' } = defineProps<Props>()
 </script>
 
 <template>
-  <Card class="group cursor-pointer border-2 hover-bg-clickable">
+  <Card class="group cursor-pointer border-2 border-surface hover-bg-clickable">
     <template #title>
-      <div class="flex justify-between">
+      <div class="flex justify-between flex-shrink truncate text-sm pr-4">
         <span>{{ title }}</span>
         <span class="group-hover-transition-right-3">
           <i class="pi pi-angle-right" style="font-size: 1.5rem"></i>
         </span>
       </div>
     </template>
-    <template #subtitle>{{ subtitle }}</template>
+    <template #subtitle>
+      <div class="flex-shrink truncate text-sm pr-4">
+        {{ subtitle }}
+      </div>
+    </template>
     <template #content>
       <p class="m-0">
         <slot name="content">content</slot>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -4,9 +4,20 @@ const user = useSupabaseClient()
 
 <template>
   <div class="mx-auto w-full max-w-[1200px] px-4 @lg:px-6 @xl:px-12 @2xl:px-20 @3xl:px-24 my-8 flex flex-col gap-8">
-    <ul class="mx-auto grid grid-cols-1 gap-2 md:gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
-      <li>
-        <DashboardCard/>
+    <div class="flex gap-4">
+      <Button size="small" variant="outlined">
+        New project
+      </Button>
+      <IconField>
+        <InputIcon class="pi pi-search" />
+        <InputText placeholder="Search for a project" size="small" />
+      </IconField>
+    </div>
+    <ul class="grid grid-cols-1 gap-2 md:gap-4 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+      <li class="w-full">
+        <DashboardCard>
+          <template #content>hi</template>
+        </DashboardCard>
       </li>
       <li>
         <DashboardCard/>
