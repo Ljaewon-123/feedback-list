5800c47b4c7897018a9d7aa461502cdb433eee77
Ljaewon-123 <jml6388@naver.com>
2025-07-16 19:34:50 +0900

    upgrade v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,6 @@ import { Noir } from './configs/prime-theme';
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  future: { compatibilityVersion: 4 },
   css: [
     '~/assets/styles/main.css',
     '~/assets/styles/inspira.css',
