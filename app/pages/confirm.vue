8b8890dd56d2d1fac18fd996c55d791cf32398e9
Ljaewon-123 <jml6388@naver.com>
2025-07-22 21:11:30 +0900

    feat: confrim page

--- app/pages/confirm.vue
+++ app/pages/confirm.vue
@@ -10,5 +10,18 @@ watch(user, () => {
 </script>
 
 <template>
-  <div>Waiting for login...</div>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+    <div class="flex flex-col items-center p-6 bg-white dark:bg-gray-800 shadow-2xl rounded-2xl">
+      <i 
+        class="pi pi-spin pi-spinner text-blue-600 dark:text-blue-400"
+        style="font-size: 4rem"
+      ></i>
+      <h1 class="mt-4 text-2xl font-semibold text-gray-900 dark:text-gray-100">
+        Waiting for login...
+      </h1>
+      <p class="mt-2 text-gray-600 dark:text-gray-400">
+        Redirecting you shortly...
+      </p>
+    </div>
+  </div>
 </template>
